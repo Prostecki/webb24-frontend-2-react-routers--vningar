530a547f171750d45ae05372831734cd2dd97401
Mark Taratynov <mark.taratynov@gmail.com>
2024-11-22 10:16:15 +0100

    styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="h-20 flex flex-col items-center justify-center bg-gray-700 text-white w-full">
+    <footer className="h-auto p-5 flex flex-col items-center justify-center bg-gray-700 text-white w-full">
       <article>All right reserved, 2024</article>
     </footer>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Home({ users }) {
   return (
-    <section className="w-full h-max text-center border text-black py-3">
+    <section className="w-full h-full text-center border text-black py-3">
       <article className="text-5xl font-extrabold">
         Welcome to User List!
       </article>
